5b66651a879f0bbbe8935fd56af01379f448a1d5
MauricioAlonso <program.alonso@gmail.com>
2024-11-24 17:10:09 -0300

    complete create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -13,13 +13,14 @@ import {
   signOutUserFailure
 } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import {Link} from "react-router-dom"
 import {
   getDownloadURL,
   getStorage,
   ref,
   uploadBytesResumable,
 } from "firebase/storage";
-import { current } from "@reduxjs/toolkit";
+
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -180,6 +181,9 @@ export default function Profile() {
         >
           {loading ? "Loading..." : "Update"}
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"}>
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
