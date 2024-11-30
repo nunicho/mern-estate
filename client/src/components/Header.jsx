4f22922ae845b7a2ae1c0eed54651aea373f45cf
MauricioAlonso <program.alonso@gmail.com>
2024-11-30 01:47:24 -0300

    complete update listing functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex-flex-wrap">
             <span className="text-slate-500">Sahand</span>
-            <span className="text-slate-700">State</span>
+            <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
